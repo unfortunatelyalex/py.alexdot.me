1739862dc45c5b47fba601b3d5e6a16db5c108ea
alex <boninalexander2@gmail.com>
2024-06-26 07:48:35 +0000

    unnecessary...

--- reflex_tmux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# navigate to the directory where the script is located (/www/wwwroot/py.alexdot.me) and run "tmux new -s reflex -d 'reflex run --env dev --frontend-port 7443 --backend-port 7444"
-
-cd /www/wwwroot/py.alexdot.me
-
-tmux new -s reflex -d 'reflex run --env dev --frontend-port 7443 --backend-port 7444'
